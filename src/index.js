60543306ecf017851702a3bb116fa46ef6ebfd79
hugstr <strh93@gmail.com>
2021-04-29 02:29:53 -0300

    learning methods and insomnia

--- src/index.js
+++ src/index.js
@@ -3,7 +3,39 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({ message: 'Hello world!' });
+    return response.json([
+        "Projeto 1",
+        "Projeto 2",
+        "Projeto 3",
+    ]);
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    return response.json([
+        "Projeto 1",
+        "Projeto 2",
+        "Projeto 3",
+        "Projeto 4",
+    ]);
+})
+
+app.put('/projects/:id', (request, response) => {
+    return response.json([
+        "Projeto 10",
+        "Projeto 2",
+        "Projeto 3",
+        "Projeto 4",
+    ]);
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        "Projeto 1",
+        "Projeto 2",
+        "Projeto 3",
+    ]);
+})
+
+app.listen(3333, () => {
+    console.log('Back-end started!🍣');
+});
