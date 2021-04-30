10de3d06ec8d63ae7edf65d99018a197d661d74a
hugstr <strh93@gmail.com>
2021-04-30 02:47:58 -0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,41 +1,71 @@
 const express = require('express');
+const {uuid} = require('uuidv4');
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
 app.get('/projects', (request, response) => {
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-        "Projeto 3",
-    ]);
+    const {title} = request.query;
+
+    const results = title
+      ? projects.filter(project => project.title.includes(title))
+      : projects;
+
+    return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-        "Projeto 3",
-        "Projeto 4",
-    ]);
+    const {title, owner} = request.body;
+
+    const project = {
+        id: uuid(),
+        title,
+        owner
+    };
+
+    projects.push(project);
+
+    return response.json(project);
 })
 
 app.put('/projects/:id', (request, response) => {
-    return response.json([
-        "Projeto 10",
-        "Projeto 2",
-        "Projeto 3",
-        "Projeto 4",
-    ]);
+    const { id } = request.params;
+    const {title, owner} = request.body;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0){
+        return response.status(400).json({ error: "Project not found. 😿"})
+    }
+
+    const project = {
+        id,
+        title,
+        owner,
+    };
+
+    projects[projectIndex] = project;
+
+    return response.json(project);
 })
 
 app.delete('/projects/:id', (request, response) => {
-    return response.json([
-        "Projeto 1",
-        "Projeto 2",
-        "Projeto 3",
-    ]);
+    const { id } = request.params;
+
+    const projectIndex = projects.findIndex(project => project.id === id);
+
+    if (projectIndex < 0){
+        return response.status(400).json({ error: "Project not found. 😿"})
+    }
+
+    projects.splice(projectIndex, 1);
+
+    return response.status(204).send(); // 204 = No Content
 })
 
 app.listen(3333, () => {
-    console.log('Back-end started!🍣');
+    console.log('Back-end started! 🍣');
 });
