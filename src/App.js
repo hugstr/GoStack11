82b4062c76bf359756b9565e9a164894d0a78b13
hugo streppel <strh93@gmail.com>
2021-05-07 17:34:33 -0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
+import api from './services/api';
+
+import './App.css';
 
 function App(){
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        // setProjects([...projects, ]);
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+	        owner: "Hugo"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
     return (
-        <>            
-            <Header title="Homepage">
-                <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                </ul>
-            </Header>
-            
-            <Header title="Projects">
+        <>                       
+            <Header title="Projects"/>
+
             <ul>
-                    <li>Homepage</li>
-                    <li>Projects</li>
-                    <li>Login</li>
-                </ul>
-            </Header>
-            
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
         </>
     );
 }
