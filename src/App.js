fa1aa10747fb18a8e6d39a7e1ec374280d2d5200
hugo streppel <strh93@gmail.com>
2021-05-07 03:55:13 -0300

    🏮

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import Header from './components/Header';
 
 function App(){
-    return <Header />;
+    return (
+        <>            
+            <Header title="Homepage">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+            
+            <Header title="Projects">
+            <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
+            
+        </>
+    );
 }
 
 export default App;
