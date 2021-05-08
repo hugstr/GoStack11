fcff6762165199d7ea90d70bf2792c5b6ac52822
hugo streppel <strh93@gmail.com>
2021-05-08 03:42:40 -0300

    added frontend

---
+++ frontend_reactjs/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import api from './services/api';
+
+import './App.css';
+
+function App(){
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        // setProjects([...projects, ]);
+        const response = await api.post('projects', {
+            title: `Novo projeto ${Date.now()}`,
+	        owner: "Hugo"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
+    return (
+        <>                       
+            <Header title="Projects"/>
+
+            <ul>
+                {projects.map(project => <li key={project.id}>{project.title}</li>)}
+            </ul>
+
+            <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
+}
+
+export default App;
