70cc52c6b6862bade727748f1c69ecd8ab6377c1
hugo streppel <strh93@gmail.com>
2021-05-17 14:42:34 -0300

    vetores, objetos e interface

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from "express-serve-static-core";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
-    return response.json({ message: "Hello World" });
+    const user = createUser({
+        email: 'strh93@gmail.com',
+        password: '123456',
+        techs: { title: 'js', experience: 100},
+    });
 }
+
