aef6d23b789d62eb7d0d2eedf67a13cda72137c9
hugstr <strh93@gmail.com>
2021-05-26 21:11:19 -0300

    user migration/user route/user service/password hash

--- iniciando_backend/src/routes/appointments.routes.ts
+++ iniciando_backend/src/routes/appointments.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
-import CreateAppointmentService from './CreateAppointmentService';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentsRouter = Router();
@@ -16,14 +16,14 @@ appointmentsRouter.get('/', async (request, response) => {
 // Rota POST
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
     const createAppointment = new CreateAppointmentService();
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
