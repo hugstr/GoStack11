392b3d55b25a16f75fe32e365254e685a4ba1dba
hugstr <strh93@gmail.com>
2021-05-29 20:12:24 -0300

    layout w/o api

--- primeiro_projeto_reactjs/src/pages/Dashboard/index.tsx
+++ primeiro_projeto_reactjs/src/pages/Dashboard/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { FiChevronRight } from "react-icons/fi";
+
+import logoImg from "../../assets/logo.svg";
+
+import { Title, Form, Repositories } from "./styles";
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
-};
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/55287730?v=4"
+            alt="Hugo Streppel"
+          />
+          <div>
+            <strong>hugstr/GoStack11</strong>
+            <p>GoStack11 classes</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 export default Dashboard;
